0bc066ac2bc23160ff4e13027d3fc35c49b83b52
pavlo.maksymov <pavlo.maksymov@spryker.com>
2023-03-14 12:31:27 +0100

    More distractions.

---
+++ database/migrations/2023_03_14_120738_add_author_reference.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAuthorReference extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'news',
+            function (Blueprint $table) {
+                $table->unsignedInteger('user_id')->nullable();
+                $table->index('user_id', 'news_user_id_index');
+                $table->foreign('user_id', 'news_user_id_foreign')
+                    ->references('id')
+                    ->on('users');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'news',
+            function (Blueprint $table) {
+                $table->dropForeign('news_user_id_foreign');
+                $table->dropIndex('news_user_id_index');
+                $table->dropColumn('user_id');
+            }
+        );
+    }
+}
